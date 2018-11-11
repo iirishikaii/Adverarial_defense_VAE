38c5fefa91660b0e720c0216d7dc1df3d8d38c1e
iirishikaii <rishika.agarwal1109@gmail.com>
2018-11-11 12:33:05 -0600

    Update form_results_pdf.py

--- form_results_pdf.py
+++ form_results_pdf.py
@@ -13,7 +13,7 @@ pdf.set_font('arial', 'B', 14.0)
 pdf.add_page()
 #pdf.cell (w = 0 , ln = 2, txt = "Comparitive Results after Adversarial Training")
 #pdf.set_font('arial', 'B', 10.0)
-pdf.cell (w = 0 , txt = "Left : for normal autoencoder ; Right : for autoencoder trained on 500 adversarial examples")
+pdf.cell (w = 0 , txt = "Left : for normal autoencoder ; Right : for autoencoder trained on 5000 adversarial examples")
 for i in range(0,15):
    
     pdf.image(dir1 + str(i) + ".png", x = 70, y = 50,  w = 370, h = 370)
@@ -23,16 +23,17 @@ for i in range(0,15):
     pdf.image(dir1 + "exp_" + str(i) + "graph2.png", x = 100, y = 680, w = 350, h = 280)
     pdf.image(dir2 + "exp_" + str(i) + "graph2.png", x = 470, y = 680, w = 350, h = 280)
     pdf.add_page()
-
+'''
 x_coord = 70
 y_coord = 30
+
 for i in range(0,15):
     
     pdf.image(dir1 + str(i) + ".png", x = x_coord, y = y_coord,  w = 250, h = 250)
     pdf.image(dir2 + str(i) + ".png", x = x_coord + 220, y = y_coord, w = 250, h = 250)
     y_coord = y_coord + 220
-
-pdfFileName = "compiled_results_svhn"
+'''
+pdfFileName = "compiled_results_svhn_5k"
 dir = "results/compare_attacks/"
 pdf.output(dir + pdfFileName + ".pdf", "F")
 
